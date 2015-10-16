f14163ec8592451482b0eb8f3694c003f415cf88
Y Hr <yvhristov@gmail.com>
2015-10-16 17:07:21 +0300

    Initial commit

--- wp-content/themes/twentysixteen/footer.php
+++
@@ -1,9 +0,0 @@
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <p>Copyright &copy; Your Website <?php echo date('Y'); ?></p>
-                </div>
-            </div>
-        </div>
-    </footer>
